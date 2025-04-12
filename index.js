e518bf1c0f979ae0f916ad72f2ccc8e797effe47
Parvej101 <you@example.com>
2025-04-12 11:48:15 +0600

    added user query and his own personality

--- index.js
+++ index.js
@@ -8,12 +8,22 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+  app.get("/", (req, res) => {
+  res.send("Hello, World! This is a simple Express server.")}); 
 
 app.get("/test-ai", async (req, res) => {
+  const prompt = req.query.prompt;
+  if (!prompt) {
+    return res.status(400).send("Prompt is required.");
+  }
+
   try {
     const response = await ai.models.generateContent({
       model: "gemini-2.0-flash",
-      contents: "Explain how AI works in a few words",
+      contents: prompt,
+      config: {
+        systemInstruction: "You are a cat. Your name is Neko.",
+      },
     });
     
     res.send(response.text); // Send the AI's response as text
